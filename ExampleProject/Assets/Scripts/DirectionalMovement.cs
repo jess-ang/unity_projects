b790407e3e74ddd78679314ee7d01e70b8e9f990
jess-ang <jessica.anguiano@uabc.edu.mx>
2020-10-24 15:56:21 -0700

    Colliders added to Character and ground

---
+++ ExampleProject/Assets/Scripts/DirectionalMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        Vector3 vector = Vector3.zero;
+        if(Input.GetKey(KeyCode.W))
+            vector += Vector3.forward;
+        if(Input.GetKey(KeyCode.A))
+            vector += Vector3.left;
+        if(Input.GetKey(KeyCode.S))
+            vector += Vector3.back;
+        if(Input.GetKey(KeyCode.D))
+            vector += Vector3.right;
+        
+        if(vector!=Vector3.zero)
+        {
+            transform.LookAt(transform.position + vector);
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            vector = Vector3.zero;
+        }
+    }
+
+
+}
