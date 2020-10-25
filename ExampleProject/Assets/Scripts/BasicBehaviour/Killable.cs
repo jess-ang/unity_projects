17aa11564c671dd62dbf51969252515db317f19a
jess-ang <jessica.anguiano@uabc.edu.mx>
2020-10-24 17:36:16 -0700

    Character moving front and back

---
+++ ExampleProject/Assets/Scripts/BasicBehaviour/Killable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Killable : MonoBehaviour
+{
+    public int health;
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        health -= 2;
+        if(health<=0)
+            Destroy(gameObject);   
+    }
+}
---
+++ ExampleProject/Assets/Scripts/BasicBehaviour/Patrol.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public int distance;
+    public float speed;
+    private Vector3 startPos,endPos,targetPos;
+    private void Awake()
+    {
+        startPos = transform.position; //Save the actual objects position
+        endPos = startPos + transform.forward * distance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position,startPos)<=0.1)
+            targetPos = endPos;
+        if(Vector3.Distance(transform.position,endPos)<=0.1)
+            targetPos = startPos;
+        transform.position = Vector3.MoveTowards(transform.position,targetPos,speed*Time.deltaTime);
+    }
+}
